d6a79c91cd49520855ec51ce387cd307552db4d1
Yury Yaroshevich <Yury_Yaroshevich@epam.com>
2016-04-21 19:57:07 +0300

    @restcontroller

--- src/main/java/com/yra/springpr/controller/EventController.java
+++ src/main/java/com/yra/springpr/controller/EventController.java
@@ -1,30 +1,28 @@
 package com.yra.springpr.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.yra.springpr.model.Event;
 import com.yra.springpr.service.EventService;
 
-@Controller
+@RestController
 public class EventController {
     @Autowired
     private EventService eventService;
     
     @RequestMapping(value = "/event", method = RequestMethod.GET)
-    public ModelAndView getEvents() {
-        ModelAndView model = new ModelAndView("jsonView");
-        model.addObject(eventService.getAll());
-        return model;
+    public List<Event> getEvents() {
+        return eventService.getAll();
     }
     
     @RequestMapping(value = "/event/{id}", method = RequestMethod.GET)
-    public ModelAndView getEvent(@PathVariable int id) {
-        ModelAndView model = new ModelAndView("jsonView");
-        model.addObject(eventService.getAll().get(id));
-        return model;
+    public Event getEvent(@PathVariable int id) {
+        return eventService.getAll().get(id);
     }
 }
